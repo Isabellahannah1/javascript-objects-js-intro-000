21b366ee2068f471be1e1f779626ba62d4ba3a82
Isabella Hannah <isabellahannah735@gmail.com>
2019-05-20 09:06:38 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,8 +1,6 @@
-var playlist = { Swift: "Youbelongtome" };
-function updatePlaylist(playlist, artistName, songTitle) { 
-  playlist[artistName]='songTitle'
-}
-function removeFromPlaylist(playlist, artistName) {
-delete playlist[artistName];
-playlist; 
+var playlist = { artistName: "songTitle" };
+
+function updatePlaylist(playlist, artistName, songTitle){
+  
+  return playlist;
 }
