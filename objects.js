8c1f5d3ecb33212fdd6a2b8fee4a9570d8526ac2
Isabella Hannah <isabellahannah735@gmail.com>
2019-05-20 09:22:36 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = { artistName: "songTitle" };
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist.slowdive = 'Alison';
+  playlist.philOchs = 'Alison';
   playlist.myBloodyValentine = 'Sometimes';
   return playlist;
 }
