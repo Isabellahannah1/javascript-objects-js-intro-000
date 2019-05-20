f9e6820fb64fca27ba14ea43461b333bce2b1e77
Isabella Hannah <isabellahannah735@gmail.com>
2019-05-20 09:09:36 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = { artistName: "songTitle" };
 
 function updatePlaylist(playlist, artistName, songTitle){
- // The body of the function should add the song and artist as a key-value pair to the playlist object.
+ artistName: "songTitle";// The body of the function should add the song and artist as a key-value pair to the playlist object.
   return playlist;
 }
